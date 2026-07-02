ca02343515a2cac62bf28a8c0509b8fdfab0f6e2
zehan <zehan9211@gmail.com>
2026-07-02 16:02:28 +0530

    chore: 🔧 configure next.js image domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["assets.aceternity.com", "pbs.twimg.com"],
+  },
 };
 
 export default nextConfig;
